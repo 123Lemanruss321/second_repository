1ca32d74298fa1ec6c5fbda2bf7bb6f441c3796d
123Lemanruss321 <123lemanruss321@gmail.com>
2024-12-20 20:35:16 +0100

    123

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+from datetime import datetime
 
 
+def string_to_date(date_string):
+   converted_date = datetime.strptime(date_string, "%Y.%m.%d").date()
+   return converted_date
+   
+
+
+def date_to_string(date):
+    converted_string = datetime.strftime(date, "%Y.%m.%d")
+    return converted_string
+
+
+date_string = "2024.01.01"
+converted_date = string_to_date(date_string)
+print(converted_date)
+date = string_to_date
+date_string = date_to_string(converted_date)
+print(date_string)
