839f9ae2c62188acb8bcda3905816fc770600a76
123Lemanruss321 <123lemanruss321@gmail.com>
2025-01-13 18:54:51 +0100

    file_system_13

--- Homework_Module_3/module3_task1.py
+++ Homework_Module_3/module3_task1.py
@@ -5,10 +5,10 @@ def get_days_from_today(date):
         input_date = datetime.strptime(date, "%Y-%m-%d")
         today_date = datetime.today()
         days_from_today = (today_date - input_date).days
-        return (print(days_from_today))
+        return days_from_today
     except ValueError:
         return (print("Date format must be 'YYYY-MM-DD'"))
     except TypeError:
         return (print("Date format must containe only digits and be in 'YYYY-MM-DD' format"))
 
-get_days_from_today("2020-10-09")
+get_days_from_today("2020.10.09")
--- Homework_Module_3/module3_task2.py
+++ Homework_Module_3/module3_task2.py
@@ -5,17 +5,17 @@ def get_numbers_ticket (min_value, max_value, quantity) -> int:
         if min_value >= 1 and max_value <= 1000:
             while len(result_array) != quantity:
                 result_array.add(randint(min_value, max_value))
-        elif min_value < 1 and max_value > 1000:
+        elif min_value < 1 or max_value > 1000:
             print ("Minimal Value must be >=1 and Maximal Value must be <=1000")
-        elif max_value > 1000:
-            print ("Maximal Value must be <=1000")
-        elif min_value < 1:
-            print ("Minimal Value must be >=1")
+            return result_array
+            #print ("Minimal Value must be >=1 and Maximal Value must be <=1000")
         result_array = sorted(list(result_array))
         return result_array
     except TypeError:
         return (print("Every Values must be only int. digits"))
+    except ValueError:
+        return
     
     
-lottery_numbers = get_numbers_ticket(1, 50, 30)
+lottery_numbers = get_numbers_ticket(10, 14, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
