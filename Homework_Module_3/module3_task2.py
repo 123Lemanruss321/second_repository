7427a4b8f776e4a9a119f89d1e45f8a54c016802
123Lemanruss321 <123lemanruss321@gmail.com>
2025-01-13 19:11:19 +0100

    file_system_13

--- Homework_Module_3/module3_task2.py
+++ Homework_Module_3/module3_task2.py
@@ -2,20 +2,18 @@ from random import randint
 def get_numbers_ticket (min_value, max_value, quantity) -> int:
     try:
         result_array = set()
-        if min_value >= 1 and max_value <= 1000:
+        if min_value >= 1 and max_value <= 1000 and max_value-min_value >= quantity:
             while len(result_array) != quantity:
                 result_array.add(randint(min_value, max_value))
-        elif min_value < 1 or max_value > 1000:
-            print ("Minimal Value must be >=1 and Maximal Value must be <=1000")
-            return result_array
-            #print ("Minimal Value must be >=1 and Maximal Value must be <=1000")
+        else:
+            return list(result_array)
         result_array = sorted(list(result_array))
         return result_array
     except TypeError:
         return (print("Every Values must be only int. digits"))
-    except ValueError:
-        return
     
     
-lottery_numbers = get_numbers_ticket(10, 14, 6)
+    
+    
+lottery_numbers = get_numbers_ticket(8, 40, 30)
 print("Ваші лотерейні числа:", lottery_numbers)
